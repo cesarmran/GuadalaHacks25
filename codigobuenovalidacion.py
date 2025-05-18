8a120e4c932ac3ce511bda28fae18b67b977f73f
EbonyValadez <123522713+EbonyValadez@users.noreply.github.com>
2025-05-17 22:33:55 -0600

    nuevocorregido

--- codigobuenovalidacion.py
+++ codigobuenovalidacion.py
@@ -44,38 +44,19 @@ def buscar_poi_con_overpass(nombre, lat, lon, radio_m=100):
     return resultados
 
 # Ejemplo de uso
-nombre = "Accesorios Chino"
-lat = 19.2710199
-lon = -99.6422219
-radio = 10  # metros
+nombre = "INSTITUTO CENCA"
+lat = 19.27045
+lon = -99.62957
+radio = 100  # metros
 
 resultados = buscar_poi_con_overpass(nombre, lat, lon, radio)
 
 if not resultados:
-    print("⚠ No se encontró nada en ese radio.")
+    print("No se encontró nada en ese radio.")
 else:
-    print(f"✅ Encontrados {len(resultados)} resultados cerca de ({lat}, {lon})")
+    print(f"Encontrados {len(resultados)} resultados cerca de ({lat}, {lon})")
     for r in resultados:
-        print(f"- {r['name']} ({r['lat']}, {r['lon']}) – {r['tipo']} ID: {r['id']}")
+        print(f"- {r['name']} ({r['lat']}, {r['lon']})")
 
-import geopandas as gpd
-from shapely.geometry import Point
 
-# Supongamos que ya tienes:
-gdf_links = gpd.read_file("/Users/ebonyvaladez/Desktop/data/STREETS_NAMING_ADDRESSING/SREETS_NAMING_ADDRESSING_4815075.geojson")  # o .shp
-# - poi: coordenadas del POI
-poi_coord = (-99.6418, 19.27052)
-punto_poi = Point(poi_coord)
 
-# Asegúrate de usar proyección en metros para calcular distancia real
-gdf_links_proj = gdf_links.to_crs(epsg=3857)
-punto_proj = gpd.GeoSeries([punto_poi], crs="EPSG:4326").to_crs(epsg=3857).iloc[0]
-
-# Calcular la distancia a todas las calles
-gdf_links_proj['dist_m'] = gdf_links_proj.distance(punto_proj)
-
-# Obtener la calle más cercana
-link_mas_cercano = gdf_links_proj.sort_values('dist_m').iloc[0]
-
-print(f"📏 Distancia mínima: {link_mas_cercano['dist_m']:.2f} metros")
-print(f"🛣 Link más cercano: {link_mas_cercano['link_id']}")
