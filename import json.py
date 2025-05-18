1a8c3241cebf7a2e66d50b2a9a7a50bb8b482436
RafaGuSua19 <rafagusua.19@gmail.com>
2025-05-17 20:28:15 -0600

    connecting poi id and coordinates and percentage

--- import json.py
+++ import json.py
@@ -1,14 +1,12 @@
 import geopandas as gpd
 
 # Ruta al archivo (puede ser .geojson o .json si es geoespacial)
-archivo = "STREETS_NAV\\SREETS_NAV_4815075.geojson"
+archivo = "C:/Users/52312/Downloads/Guadalahacks25/STREETS_NAMING_ADDRESSING/SREETS_NAMING_ADDRESSING_4815083.geojson"
 
 # Leer el archivo
 gdf = gpd.read_file(archivo)
 
 # Mostrar las primeras filas
 print(gdf.head())
-
-# Guardar como CSV
-gdf.to_csv("streets_nav_output.csv", index=False)
+gdf.to_csv("nav1.csv", index=False)
 
